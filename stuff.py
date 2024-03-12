ee2d80fac681f92e87dbecb792d2a5fc8f5e92e5
Johan Liebert <163127664+JohanLiebert-II@users.noreply.github.com>
2024-03-12 11:37:22 +0000

    git commit -a

--- stuff.py
+++ stuff.py
@@ -1,1 +1,12 @@
-print("Hi")
+import time
+import random
+
+time.sleep(0.7)
+
+print('''
+      - ===== - Menu - ===== - 
+      1. BlackJack
+      2. Coding Quiz
+      3. Number Guess
+      4. Calculator
+''')
