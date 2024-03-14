c7575adb55d2b55a668c30ea0de3948f8120a605
Johan Liebert <163127664+JohanLiebert-II@users.noreply.github.com>
2024-03-14 19:52:39 +0000

    git commit -a

--- Password_checker.py
+++ Password_checker.py
@@ -1,7 +1,14 @@
+import sys
+
 print('''
       - === - Menu - === -
       1.Password Checker 
       2.Quit
       ''')
 
-if 
+menu_selection = int(input("Please enter menu selection > ")) 
+
+if menu_selection == 1:
+    print("Password")
+if menu_selection ==2:
+    sys.exit      
